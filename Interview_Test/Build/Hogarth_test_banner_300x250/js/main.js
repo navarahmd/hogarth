6d06a32e9c82e085876cb5e2799c8a594d822385
Navar Ahammed <53256284+navarahmd@users.noreply.github.com>
2022-07-22 21:31:46 +0530

    Update main.js

--- Interview_Test/Build/Hogarth_test_banner_300x250/js/main.js
+++ Interview_Test/Build/Hogarth_test_banner_300x250/js/main.js
@@ -4,7 +4,7 @@
 
 
 // new
-var tl = gsap.timeline({ onComplete: someFunction });
+var tl = gsap.timeline({ onComplete: showRefreshbutton });
 
 
 // copy 
@@ -90,52 +90,12 @@ function buildTimeline() {
 
 
 function buildTimeline1() {
-    console.log("buildTimeline new");
-
-    tl.addLabel("frame01", 0)
-
-    // show frame content
-    .to([copyFrame01], { duration: _fadeInSpeed, alpha: 1, ease: "none" }, "frame01+=" + _fadeOutSpeed);
-
-
-    tl.addLabel("frame02", 1.5)
-
-    // remove prev frame content
-    .to([copyFrame01], { duration: _fadeOutSpeed, alpha: 0, ease: "none" }, "frame02")
-
-    // show frame content
-    .to([copyFrame02], { duration: _fadeInSpeed, alpha: 1, ease: "none" }, "frame02+=" + _fadeOutSpeed);
-
-    tl.addLabel("frame03", 3)
-
-    // remove prev frame content
-    .to([copyFrame02], { duration: _fadeOutSpeed, alpha: 0, ease: "none" }, "frame03")
-
-    // show frame content
-    .to([copyFrame03], { duration: _fadeInSpeed, alpha: 1, ease: "none" }, "frame03+=" + _fadeOutSpeed);
-
-
-    tl.addLabel("frame04", 4.5)
-
-    // remove prev frame content
-    .to([copyFrame03], { duration: _fadeOutSpeed, alpha: 0, ease: "none" }, "frame04")
-
-    // show frame content
-    .to([copyFrame04], { duration: _fadeInSpeed, alpha: 1, ease: "none" }, "frame04+=" + _fadeOutSpeed);
-
-
-    tl.addLabel("frame05", 6)
-
-    // remove prev frame content
-    .to([copyFrame04], { duration: _fadeOutSpeed, alpha: 0, ease: "none" }, "frame05")
-
-    // show frame content
-    .to([copyFrame05], { duration: _fadeInSpeed, alpha: 1, ease: "none" }, "frame05+=" + _fadeOutSpeed)
-        .to([ctaCopy, ctaCircle, ctaArrow], { duration: _fadeInSpeed, alpha: 1, ease: "none" }, "frame05+=" + _fadeOutSpeed);
-
-
+    var t2 = new TimelineMax();
+    t2.to("#btn", 1, { rotation: 360 });
+    tl.restart();
 }
 
-function someFunction() {
+function showRefreshbutton() {
+
     document.getElementById("btn").style.display = "block";
 }
